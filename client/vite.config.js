d5f7ec1d024ebc20a92b86805ed677654ead6c63
Sk Sazid <1.sksazid@gmail.com>
2025-06-09 22:04:12 +0600

    Client Port changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5174,
+    port: 5180,
     strictPort: true,
     proxy: {
       '/v1': {

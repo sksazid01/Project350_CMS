e38cc0b694c0b95a526979336dde57ec618a7bb6
Sk Sazid <1.sksazid@gmail.com>
2025-06-09 22:05:42 +0600

    Client fails to login in Different port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5180,
+    port: 5174,
     strictPort: true,
     proxy: {
       '/v1': {
